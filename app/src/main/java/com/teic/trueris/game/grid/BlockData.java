40bdac5a1909d85450e67da1667904b963eecf8e
Terrance1120 <iterranceevan@gmail.com>
2025-10-02 23:14:20 +0800

    Commit 2025-10-02 #1

--- app/src/main/java/com/teic/trueris/game/grid/BlockData.java
+++ app/src/main/java/com/teic/trueris/game/grid/BlockData.java
@@ -1,11 +1,20 @@
 package com.teic.trueris.game.grid;
 
+import com.teic.trueris.game.templates.BlockTemplate;
 import com.teic.trueris.game.templates.CellTemplate;
 
 public class BlockData {
-    private CellTemplate[][] block;
+    private final CellTemplate[][] block;
+    private Direction blockRotation = Direction.UP;
     private int blockRow;
     private int blockCol;
+
+    /*private CellTemplate[][] prevBlockRotation = 
+        BlockTemplate.EMPTY_BLOCK;;*/
+
+    private Direction prevBlockRotation = blockRotation;
+    private int prevBlockRow;
+    private int prevBlockCol;
 
     // Let BlockData handle BlockCollision. GridManager does 
     // not need to know the specific implementations of how 
@@ -20,11 +29,121 @@ public class BlockData {
     // to grid. Not sure if it is a good idea or if I should 
     // let GridManager do that instead. But it makes things, 
     // uh, simpler, *maybe* 
-
-
-    public boolean moveDown() {
-
+    
+    public BlockData(CellTemplate[][] block) {
+        this.block = block;
     }
 
+    public void moveDown() {
+        prevBlockRow = blockRow;
+        blockRow++;
+    }
+
+    public void moveLeft() {
+        prevBlockCol = blockCol;
+        blockCol--;
+    }
+
+    public void moveRight() {
+        prevBlockCol = blockCol;
+        blockCol++;
+    }
+
+    public void rotateLeft() {
+        Direction[] directions = Direction.values();
+
+        int rotationIndex = (
+            ((blockRotation.ordinal() - 1) % 4 + 4) % 4
+        );
+
+        prevBlockRotation = blockRotation;
+        
+        blockRotation = directions[rotationIndex];
+    }
+
+    public void rotateRight() {
+        Direction[] directions = Direction.values();
+
+        int rotationIndex = (
+            ((blockRotation.ordinal() + 1) % 4 + 4) % 4
+        );
+
+        prevBlockRotation = blockRotation;
+        
+        blockRotation = directions[rotationIndex];
+    }
+        
+    public void revertPosition() {
+        blockRow = prevBlockRow;
+        prevBlockRow = 0;
+
+        blockCol = prevBlockCol;
+        prevBlockCol = 0;
+    }
+
+    
+    public void revertBlockRotation() {
+        blockRotation = prevBlockRotation;
+        prevBlockRotation = Direction.UP;
+    }
+
+    public CellTemplate[][] getRotatedBlockCopy() {
+        return rotateBlockNTimes(blockRotation.ordinal());
+    }
+
+    private CellTemplate[][] rotateBlockNTimes(int amount) {
+        CellTemplate[][] newBlock = deepCopy(block);
+
+        for (int i = 0; i < amount; i++) {
+            newBlock = rotateArrayRight(newBlock);
+        }
+
+        return newBlock;
+    }
+
+    private CellTemplate[][] rotateArrayRight(
+        CellTemplate[][] block
+    ) {
+        int blockSize = block.length;
+        CellTemplate[][] newBlock = 
+            new CellTemplate[blockSize][blockSize];
+
+        for (int row = 0; row < blockSize; row++) {
+            for (int col = 0; col < blockSize; col++) {
+                newBlock[row][blockSize - 1 - col] = 
+                    block[col][row];
+            }
+        }
+
+        return newBlock;
+    }
+
+    private CellTemplate[][] deepCopy(
+        CellTemplate[][] original
+    ) {
+        int blockSize = original.length;
+        CellTemplate[][] copy = 
+            new CellTemplate[blockSize][blockSize];
+
+        for (int row = 0; row < blockSize; row++) {
+            for (int col = 0; col < blockSize; col++) {
+                copy[row][col] = original[row][col];
+            }
+        }
+
+        return copy;
+    }
+
+    public int blockRow() {
+        return blockRow;
+    }
+
+    public int blockCol() {
+        return blockCol;
+    }
+}
+
+enum Direction {
+    UP, RIGHT, DOWN, LEFT;
 }
 
--- app/src/main/java/com/teic/trueris/game/grid/Collision.java
+++ app/src/main/java/com/teic/trueris/game/grid/Collision.java
@@ -1,40 +1,43 @@
 package com.teic.trueris.game.grid;
 
 import com.teic.trueris.Config;
+import com.teic.trueris.game.templates.CellTemplate;
 
-public class BlockCollision {
-    private Config config;
-    private GridData gridData;
+public class Collision {
+    private final Config config;
+    private final GridData gridData;
 
-    public BlockCollision(Config config, GridData gridData) {
+    public Collision(Config config, GridData gridData) {
         this.config = config;
         this.gridData = gridData;
     }
 
-    public boolean isValid(BlockManager blockManager) {
-        int blockSize = blockManager.getBlockSize();
-        int blockRowPos = blockManager.blockRowPos();
-        int blockColPos = blockManager.blockColPos();
+    public boolean isValid(BlockData blockData) {
+        CellTemplate[][] block = blockData.getRotatedBlockCopy();
+
+        int blockSize = block.length;
+        int blockRow = blockData.blockRow();
+        int blockCol = blockData.blockCol();
 
         for (int row = 0; row < blockSize; row++) {
             for (int col = 0; col < blockSize; col++) {
-                if (blockManager.getCell(row, col).isEmpty()) {
+                if (block[row][col].isEmpty()) {
                     continue;
                 }
 
-                int gridRow = blockRowPos + row;
-                int gridCol = blockColPos + col;
+                int gridRow = blockRow + row;
+                int gridCol = blockCol + col;
 
                 if (
                     isOutOfBounds(gridRow, gridCol)
                     || isColliding(gridRow, gridCol)
                 ) {
-                    return true;
+                    return false;
                 }
             }
         }
 
-        return false;
+        return true;
     }
 
     private boolean isOutOfBounds(int gridRow, int gridCol) {
